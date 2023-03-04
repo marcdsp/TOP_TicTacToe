4d7bec1f30138baa6d0fcc8a9d1cebe7b9b5aa00
Marc <marcdsp@gmail.com>
2023-03-03 20:39:52 -0600

    Basic file structure and gameboard divs created

---
+++ main.js
@@ -0,0 +1,12 @@
+const calculator = (() => {
+    const cellA = document.getElementById(cellA);
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
